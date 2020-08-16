080fbf9ca1058ea97029844778b443eb13ce982b
sivanharel <sivanh@wix.com>
2020-08-16 12:42:38 +0300

    add some more visual tests

--- src/TestimonialList/test/TestimonialList.visual.js
+++ src/TestimonialList/test/TestimonialList.visual.js
@@ -35,6 +35,72 @@ const tests = [
         it: 'default',
         props: {},
       },
+      {
+        it: 'no avatar',
+        props: {
+          testimonials: [
+            {
+              id: '0001',
+              text: 'I love it! This product is exactly what I needed.',
+              authorName: 'Guy in glasses',
+            },
+            {
+              id: '0002',
+              text: 'Amazing! It helped me to solve my problems.',
+              authorName: 'Person with a hat',
+            },
+            {
+              id: '0003',
+              text: 'A perfect tool for my every day tasks.',
+              authorName: 'Smiling lady',
+            },
+          ],
+        },
+      },
+      {
+        it: 'no text',
+        props: {
+          testimonials: [
+            {
+              id: '0001',
+              avatar: <Avatar name="Guy in glasses" size="size60" />,
+              authorName: 'Guy in glasses',
+            },
+            {
+              id: '0002',
+              avatar: <Avatar name="Person with a hat" size="size60" />,
+              authorName: 'Person with a hat',
+            },
+            {
+              id: '0003',
+              avatar: <Avatar name="Smiling lady" size="size60" />,
+              authorName: 'Smiling lady',
+            },
+          ],
+        },
+      },
+      {
+        it: 'no authorName',
+        props: {
+          testimonials: [
+            {
+              id: '0001',
+              avatar: <Avatar name="Guy in glasses" size="size60" />,
+              text: 'I love it! This product is exactly what I needed.',
+            },
+            {
+              id: '0002',
+              avatar: <Avatar name="Person with a hat" size="size60" />,
+              text: 'Amazing! It helped me to solve my problems.',
+            },
+            {
+              id: '0003',
+              avatar: <Avatar name="Smiling lady" size="size60" />,
+              text: 'A perfect tool for my every day tasks.',
+            },
+          ],
+        },
+      },
     ],
   },
 ];
