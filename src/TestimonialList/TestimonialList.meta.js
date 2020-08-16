e1bb22a25b283b164de8c2df34518bf3cb1762eb
sivanharel <sivanh@wix.com>
2020-08-16 11:46:51 +0300

    migration to stylable 3 & fix metadata file

--- src/TestimonialList/TestimonialList.meta.js
+++ src/TestimonialList/TestimonialList.meta.js
@@ -3,6 +3,6 @@ import Registry from '@ui-autotools/registry';
 
 const metadata = Registry.getComponentMetadata(TestimonialList);
 
-metadata.exportInfo({
+metadata.exportInfo = {
   path: 'src/TestimonialList/TestimonialList.js',
-});
+};
