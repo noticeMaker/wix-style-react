686097226b89b0a7f2ce360a0eab448c570dafb9
Egozi <egozi@wix.com>
2020-08-17 11:24:37 +0300

    migrate to Stylable3

--- src/TableListItem/TableListItem.js
+++ src/TableListItem/TableListItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { dataHooks } from './constants';
-import styles from './TableListItem.st.css';
+import { st, classes } from './TableListItem.st.css';
 import Checkbox from '../Checkbox';
 import Box from '../Box';
 import DragHandle from 'wix-ui-icons-common/system/DragAndDropLarge';
@@ -44,8 +44,8 @@ const TableListItem = ({
   return (
     <div
       onClick={onClick}
-      {...styles(
-        'root',
+      className={st(
+        classes.root,
         {
           draggable: draggable && !dragDisabled,
           checked: checkbox && checked,
@@ -53,14 +53,14 @@ const TableListItem = ({
           clickable: !!onClick,
           ...{ verticalPadding },
         },
-        { className },
+        className,
       )}
       data-hook={dataHook}
     >
       <Box>
         {draggable && (
           <div
-            {...styles('dragHandle', {
+            className={st(classes.dragHandle, {
               disabled: dragDisabled,
             })}
             data-hook={dataHooks.tableListItemDragHandle}
@@ -70,7 +70,7 @@ const TableListItem = ({
         )}
         {checkbox && (
           <div
-            className={styles.checkbox}
+            className={classes.checkbox}
             data-hook={dataHooks.tableListItemCheckboxContainer}
             onClick={onCheckboxChange}
           >
@@ -82,7 +82,7 @@ const TableListItem = ({
           </div>
         )}
         <Box
-          className={styles.optionsContainer}
+          className={classes.optionsContainer}
           style={{
             gridTemplateColumns: getWidthStyle(options),
           }}
@@ -91,7 +91,9 @@ const TableListItem = ({
         >
           {options.map(({ value, align }, index) => (
             <div
-              {...styles('align', { position: ALIGN[align] || ALIGN.left })}
+              className={st(classes.align, {
+                position: ALIGN[align] || ALIGN.left,
+              })}
               key={index}
               data-hook={dataHooks.tableListItemValue}
             >
--- src/TableListItem/TableListItem.meta.js
+++ src/TableListItem/TableListItem.meta.js
@@ -3,6 +3,6 @@ import Registry from '@ui-autotools/registry';
 
 const metadata = Registry.getComponentMetadata(TableListItem);
 
-metadata.exportedFrom({
+metadata.exportInfo = {
   path: 'src/TableListItem/TableListItem.js',
-});
+};
